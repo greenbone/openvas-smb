608691c8420b5fc51b40e736b8d841bf70745d3e
tuxmaster <tuxmaster@users.noreply.github.com>
2018-03-28 14:15:47 +0200

    Fix for using heimdal 7.5.0

--- samba/auth/auth_util.c
+++ samba/auth/auth_util.c
@@ -23,7 +23,7 @@
 */
 
 #include "includes.h"
-#include <gssapi/gssapi.h>
+#include <heimdal/gssapi/gssapi.h>
 #include "auth/auth.h"
 #include "libcli/security/security.h"
 #include "libcli/auth/libcli_auth.h"
