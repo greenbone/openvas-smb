00741ed8ef723d886e955e93c2a5019dda8da0f6
Christop Kraemer <christoph.kraemer@greenbone.net>
2023-05-22 11:12:36 +0200

    Fix: Potentially uninitialized local variable
    A local non-static variable of a non-class type has an undefined value before it is initialized. For example, it is incorrect to rely on an uninitialized integer to have the value `0`.
    
    Fixed by initialize local variable after declaration.

--- samba/auth/gensec/gensec_krb5.c
+++ samba/auth/gensec/gensec_krb5.c
@@ -497,7 +497,7 @@ static NTSTATUS gensec_krb5_session_key(struct gensec_security *gensec_security,
 	struct gensec_krb5_state *gensec_krb5_state = gensec_security->private_data;
 	krb5_context context = gensec_krb5_state->smb_krb5_context->krb5_context;
 	krb5_auth_context auth_context = gensec_krb5_state->auth_context;
-	krb5_keyblock *skey;
+	krb5_keyblock *skey = NULL;
 	krb5_error_code err = -1;
 
 	if (gensec_krb5_state->session_key.data) {
